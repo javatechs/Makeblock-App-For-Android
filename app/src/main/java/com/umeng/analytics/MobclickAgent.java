d6acc516c912e1eefcbca5259a161faba034a36d
javatechs <javatechs@gmail.com>
2018-11-12 09:47:28 -0800

    Add stub analytics implementation.

---
+++ app/src/main/java/com/umeng/analytics/MobclickAgent.java
@@ -0,0 +1,13 @@
+/**
+ * Stub implementation
+ */
+package com.umeng.analytics;
+
+import android.app.Activity;
+import android.content.Context;
+
+public class MobclickAgent {
+    public static void onPause(Activity activity) {}
+    public static void onResume(Activity activity) {}
+    public static void onEvent(Context context, String str) {}
+}
